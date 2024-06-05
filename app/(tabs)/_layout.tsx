9abbfe854b31b29f32e8798f9cf66407e6567f2c
张殃离 <96040653+ZhangYangLizzm@users.noreply.github.com>
2024-06-05 23:52:16 +0800

    feat: update - nativewind - tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,34 +1,49 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+import { View, Text } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+  const insets = useSafeAreaInsets();
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
+        tabBarActiveTintColor: "blue",
+        header: () => (
+          <View className="bg-white" style={{ paddingTop: insets.top }}>
+            <View className="h-8 flex justify-center items-center ">
+              <Text className="font-bold text-base2">MoneyTracker</Text>
+            </View>
+          </View>
+        ),
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="add"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          tabBarLabel: () => null,
+          tabBarIcon: () => (
+            <View className="bg-blue-600 rounded-full w-[40px] h-[40px] flex justify-center items-center">
+              <FontAwesome size={24} name="plus" color="white" />
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
           ),
         }}
       />
--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Tab() {
   return (
     <View className="justify-center items-center flex-1">
-      <Text>Tab Home</Text>
+      <Text>Tab add</Text>
     </View>
   );
 }
